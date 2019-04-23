71f84fd160e7870d648b4416a033c9ce5a8bc7e8
sb8ay <sb8ay@mathcs05.richmond.edu>
2019-04-23 19:36:40 -0400

    road updated

--- Road.cpp
+++ Road.cpp
@@ -19,37 +19,57 @@ Road::~Road()
 {
 }
 
-
+bool Road::getArrival(probNorth, probSouth, probEast, probWest)
+{
+  float totalProb = rrand() % 1 + 0;
+  if(totalProb <= .1)
+    { //add as northbound
+      return true;
+    }
+  else if(totalProb <=.2)
+    { //add as south boun
+      return true
+    }
+  else if(totalProb <= .35)
+    {
+     //add as eastbound
+     return true;
+    }
+  else if(totalProb <= .5)
+    {
+     // add as westbound
+     return true;
+    }
+  else
+     return false; 
+}
 direction Road::getDirection()
 {
   return direction;
-}
+};
+
 
 
 void Road::advance()
 { 
   Section* c;
-
-  if(TrafficLight::getStatus() == green || TrafficLight::getStatus() == yellow)
-    {
        for(int i = 0; i < vehicles.size(); i++)
          {
 	   c = vehicles.at(i).getVehicle();
-	   if(!(*c)+1.Section::getOccupied()  && Section::)
+	   if(!(*c)+1.Section::getOccupied())
 	     { 
 	       Section::vehicle.at(i).setVehicle((*c)+1);
-	     }	
-       TrafficLight::updateLight();
+	     }	       
          }
-    }
-
-  else
-     {
-        if(!Section::getOccupied())
-	{
-	  //logic to move ahead
-	}
-	TrafficLight::updateLight();
-        }
-     }
 };
+
+
+     
+        
+
+	  
+
+	
+        
+     
+
