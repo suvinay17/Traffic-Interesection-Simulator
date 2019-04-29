d73eceec61a5bc444955bfd3ec138e2d4a671b2b
ad9hi <47748530+ad9hi@users.noreply.github.com>
2019-04-28 22:45:07 -0400

    comments

--- Section.cpp
+++ Section.cpp
@@ -1,15 +1,36 @@
+//Authors: Allie, Ting, Suvinay
+//Last edit: April 28, 2019
+//Implementation of sections for lane, it holds vehiclePtr and the
+// status for the section
+
+
 #include "Section.h"
 
+//constructor ititializes isOccupied, atIntersection, vehiclePtr
 Section::Section(bool isO, bool atI, VehicleBase* v){
     isOccupied = isO;
     atIntersection = atI;
     vehiclePtr = v;
 }
+
+//sets the bool isOccupied
 void Section::setPtr(VehicleBase* ptr){
     if (ptr != nullptr)
         isOccupied = true;
-    else 
+    else
         isOccupied = false;
-
     vehiclePtr = ptr;
 }
+
+//creates a destructor
+Section::~Section(){}
+
+//returns a pointer to the vehicle
+VehicleBase* getPtr(){
+    return vehiclePtr;
+    }
+
+//returns the status of the section
+bool getOccupied(){
+    return isOccupied;
+    }
