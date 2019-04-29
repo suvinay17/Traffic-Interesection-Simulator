3bdc30dbdfe3bb06163823a8d69c5118d1a2168f
sb8ay <sb8ay@mathcs04.richmond.edu>
2019-04-29 11:30:28 -0400

    changed a comment

--- Lane.cpp
+++ Lane.cpp
@@ -109,7 +109,7 @@ void Lane::advanceRed() // with more cars, advance only up to stop point
 
         int i = 0;
 
-        while (i < (int)sections.size() - 4){ //largest vehicle is car
+        while (i < (int)sections.size() - 4){ //largest vehicle is truck 
             int j = i + 1;
 
             while (sections[i].getOccupied() && sections[j].getOccupied() && j != (int)sections.size() - 2){
